488edbc54b0df2f9b036a55f6c543234368bc531
eng-arvind <arvindvishwakarma500@gmail.com>
2021-09-21 11:17:14 +0530

    hello

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const hostname = "localhost";
+const port = 3000;
+const server = http.createServer((req, res) => {
+    // console.log(req.headers);
+    console.log('request for ' + req.url + 'by method' + req.method);
+
+    if (req.method == 'GET') {
+        var fileURL;
+        if (req.url == '/') {
+            fileURL = "/index.html"
+        }
+        else {
+            fileURL = req.url;
+        }
+        var filePath = path.resolve('./public' + fileURL);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><h1>error 404:' + fileURL + ' does not exists </h1></html>');
+                }
+
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                    
+            })
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><h1>error 404:' + fileURL + '.js not a html file </h1></html>');
+        }
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><h1>error 404:' + fileURL + 'not supported </h1></html>');
+    }
+
+});
+server.listen(port, hostname), () => {
+    console.log(`server running at http://${hostname}:${port}`);
+};
